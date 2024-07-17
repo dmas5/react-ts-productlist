2ee1c370e260473652ffb27c3ad350a4a07f7cc7
dmas5 <artur.unt@gmail.com>
2024-07-17 22:23:29 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,37 @@ function App() {
   const [products, setProducts] = useState<Product[]>([]);
 
   const addProduct = (product: Product): void => {
+
     setProducts([...products,product]);
+
   };
   const removeProduct = (id: number): void => {
+
     setProducts(products.filter(p => p.id !== id));
+
   };
   const toggleEdit = (id: number): void => {
+
     const productToEdit = products.find(p => p.id === id)!;
     const editedProduct = {...productToEdit, edit : !productToEdit.edit};
     setProducts(products.map(p => p.id !== id ? p: editedProduct));
+
   };
   const saveEditedProduct = (product: Product): void => {
+
     setProducts(products.map(p => p.id !== product.id ? p: product));
+    
   };
   
 
   return (
     <div>
       <ProductForm addProduct={addProduct} />
-      <ProductList 
-      products={products}
-      removeProduct={removeProduct}
-      toggleEdit={toggleEdit}
-      saveEditedProduct={saveEditedProduct}
+      <ProductList
+        products={products}
+        removeProduct={removeProduct}
+        toggleEdit={toggleEdit}
+        saveEditedProduct={saveEditedProduct}
       />
     </div>
   )
